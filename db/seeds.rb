cdd91a744715abb613d52d1373da3b1eab91e65a
Carl Camit <carlcamit0@gmail.com>
2017-10-11 16:12:19 +1100

    fixed another typo in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-Listing.create!(title: "Coder Academy", street_address: "120 Spencer St.", city: "Melbourne", country_code: "au", bed_count: 35, bedroom_count: 1, bathroom_count: 3, description: "Cozy to code", night_fee_cents: 5000, cleaning_fee_cents: 1000)
-Listing.create!(title: "Empire State Building", street_address: "338-350 5th Ave", city: "New York", country_code: "us", bed_count: , bedroom_count: , bathroom_count: , description: "", night_fee_cents: , cleaning_fee_cents: )
-Listing.create!(title: "Champ de Mars", street_address: "2 Allée Adrienne Lecouvreur", city: "Paris", country_code: "fr", bed_count: , bedroom_count: , bathroom_count: , description: "", night_fee_cents: , cleaning_fee_cents: )
-Listing.create!(title: "Leaning Tower of Pisa", street_address: "Piazza del Duomo", city: "Pisa", country_code: "it", bed_count: , bedroom_count: , bathroom_count: , description: "", night_fee_cents: , cleaning_fee_cents: )
+Listing.create!(title: "Coder Academy" street_address: "120 Spencer St." city: "Melbourne" country_code: "au" bed_count: 35 bedroom_count: 1 bathroom_count: 3 description: "Cozy to code" night_fee_cents: 5000 cleaning_fee_cents: 1000)
+Listing.create!(title: "Empire State Building" street_address: "338-350 5th Ave" city: "New York" country_code: "us" bed_count: 2 bedroom_count: 2 bathroom_count: 12 description: "What the hell" night_fee_cents: 4000 cleaning_fee_cents: 20000)
+Listing.create!(title: "Champ de Mars" street_address: "2 Allée Adrienne Lecouvreur" city: "Paris" country_code: "fr" bed_count: 4 bedroom_count: 3 bathroom_count: 5 description: "Is happening" night_fee_cents: 7000 cleaning_fee_cents: 3000)
+Listing.create!(title: "Leaning Tower of Pisa" street_address: "Piazza del Duomo" city: "Pisa" country_code: "it" bed_count: 5 bedroom_count: 2 bathroom_count: 2 description: "Over here" night_fee_cents: 2000 cleaning_fee_cents: 2000)
 
