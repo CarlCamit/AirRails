64223d7fc7d5f49d71d48b0dcfd1ae9f25dd24bd
Carl Camit <carlcamit0@gmail.com>
2017-10-11 16:02:21 +1100

    fixed typo in method added test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Listing.create!(title: "Coder Academy", street_address: "120 Spencer St.", city: "Melbourne", country_code: "au", bed_count: 35, bedroom_count: 1, bathroom_count: 3, description: "Cozy to code", night_fee_cents: 5000, cleaning_fee_cents: 1000)
+Listing.create!(title: "Empire State Building", street_address: "338-350 5th Ave", city: "New York", country_code: "us", bed_count: , bedroom_count: , bathroom_count: , description: "", night_fee_cents: , cleaning_fee_cents: )
+Listing.create!(title: "Champ de Mars", street_address: "2 Allée Adrienne Lecouvreur", city: "Paris", country_code: "fr", bed_count: , bedroom_count: , bathroom_count: , description: "", night_fee_cents: , cleaning_fee_cents: )
+Listing.create!(title: "Leaning Tower of Pisa", street_address: "Piazza del Duomo", city: "Pisa", country_code: "it", bed_count: , bedroom_count: , bathroom_count: , description: "", night_fee_cents: , cleaning_fee_cents: )
+
