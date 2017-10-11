1c26498f818550e21599147ac6e35992eb3fe0ce
Carl Camit <carlcamit0@gmail.com>
2017-10-11 15:33:47 +1100

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root 'listings#index'
 
 end
